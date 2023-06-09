16b1fc88e3fafe01406e56a9c308ba0d103bbf26
Joannaaaaaa <98182630+Joannaaaaaa@users.noreply.github.com>
2023-06-09 20:23:10 +0800

    Update group_by_aspect_ratio.py

--- Keypoint_Detection/group_by_aspect_ratio.py
+++ Keypoint_Detection/group_by_aspect_ratio.py
@@ -13,19 +13,7 @@ from PIL import Image
 
 
 class GroupedBatchSampler(BatchSampler):
-    """
-    Wraps another sampler to yield a mini-batch of indices.
-    It enforces that the batch only contain elements from the same group.
-    It also tries to provide mini-batches which follows an ordering which is
-    as close as possible to the ordering from the original sampler.
-    Arguments:
-        sampler (Sampler): Base sampler.
-        group_ids (list[int]): If the sampler produces indices in range [0, N),
-            `group_ids` must be a list of `N` ints which contains the group id of each sample.
-            The group ids must be a continuous set of integers starting from
-            0, i.e. they must be in the range [0, num_groups).
-        batch_size (int): Size of mini-batch.
-    """
+    
     def __init__(self, sampler, group_ids, batch_size):
         if not isinstance(sampler, Sampler):
             raise ValueError(
@@ -51,15 +39,9 @@ class GroupedBatchSampler(BatchSampler):
                 del buffer_per_group[group_id]
             assert len(buffer_per_group[group_id]) < self.batch_size
 
-        # now we have run out of elements that satisfy
-        # the group criteria, let's return the remaining
-        # elements so that the size of the sampler is
-        # deterministic
         expected_num_batches = len(self)
         num_remaining = expected_num_batches - num_batches
         if num_remaining > 0:
-            # for the remaining batches, take first the buffers with largest number
-            # of elements
             for group_id, _ in sorted(buffer_per_group.items(),
                                       key=lambda x: len(x[1]), reverse=True):
                 remaining = self.batch_size - len(buffer_per_group[group_id])
